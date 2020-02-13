38f84b71f16172a2e7c646e27088d9f1fddf314b
zoukh <907022598@qq.com>
2020-02-13 22:44:36 +0800

    gitignore

---
+++ src/main/java/com/shetuan/util/MD5Utils.java
@@ -0,0 +1,13 @@
+package com.shetuan.util;
+
+/**
+ * use for :
+ *
+ * @author zoukh
+ * Created in:  2020/2/13 13:49
+ * @version 1.0
+ * @Modified By:
+ * @used in: community-management-system
+ */
+public class MD5Utils {
+}
---
+++ src/main/java/com/shetuan/util/TestMain.java
@@ -0,0 +1,13 @@
+package com.shetuan.util;
+
+/**
+ * use for :
+ *
+ * @author zoukh
+ * Created in:  2020/2/13 15:01
+ * @version 1.0
+ * @Modified By:
+ * @used in: community-management-system
+ */
+public class TestMain {
+}
